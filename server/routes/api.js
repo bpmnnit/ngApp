30b3384fac8d5e442312adfd850c048b91fd3cb0
Bhanu Pratap Singh <bp.mnnit@gmail.com>
2019-11-25 15:15:58 +0530

    Modified server.js and created api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,3 +1,12 @@
 /**
  * Created by CGS_01 on 25-11-2019.
  */
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.send('Api Works!');
+});
+
+module.exports = router;
